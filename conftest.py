be16d49339d52fa0f19ccea41efa57555f1fd499
Sharmaine <sharmainebernardo@gmail.com>
2025-08-09 21:50:23 +0800

    some updates

--- conftest.py
+++ conftest.py
@@ -2,11 +2,13 @@ import pytest
 from playwright.sync_api import sync_playwright, Page
 from tests.pages.contact_page import ContactPage
 from tests.pages.home_page import HomePage
+from tests.pages.shop_page import ShopPage
+from tests.pages.cart_page import CartPage
 
 @pytest.fixture(scope="session")
 def page():
     with sync_playwright() as p:
-        browser = p.chromium.launch(headless=True)
+        browser = p.chromium.launch(headless=False)
         context = browser.new_context()
         page = context.new_page()
         yield page
@@ -19,3 +21,11 @@ def home_page(page: Page) -> HomePage:
 @pytest.fixture()
 def contact_page(page: Page) -> ContactPage:
     return ContactPage(page)
+
+@pytest.fixture()
+def shop_page(page: Page) -> ShopPage:
+    return ShopPage(page)
+
+@pytest.fixture()
+def cart_page(page: Page) -> CartPage:
+    return CartPage(page)
